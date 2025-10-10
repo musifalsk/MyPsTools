8f09e4bda8e7f4dd94f2acf295893a7217fa31af
ole.gjose <ogjo@equinor.com>
2025-10-10 14:21:28 +0200

    refactor: replace Get-Random with Get-SecureRandom for enhanced security in password generation

--- PSModules/RandomPassword/RandomHexKey.ps1
+++ PSModules/RandomPassword/RandomHexKey.ps1
@@ -27,10 +27,9 @@ function Get-RandomHexKey {
     )
 
     $chars = @()
-    do { $chars += ('a'..'f' | Get-Random -Count 1) } until ($chars.Length -gt ($Length * 1 / 4 - 1))
-    do { $chars += ('0'..'9' | Get-Random -Count 1) } until ($chars.Length -gt ($Length * 4 / 4 - 1))
-    #do { $chars += ('!', '@', '#', '$', '%', '&', '=', '+', '-', '?', '^', '*', '.', ',' | Get-Random -Count 1) } until ($chars.Length -gt ($Length - 1))
-    $pw = -join ($chars | Sort-Object { Get-Random })
+    do { $chars += ('a'..'f' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length * 1 / 4 - 1))
+    do { $chars += ('0'..'9' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length * 4 / 4 - 1))
+    $pw = -join ($chars | Sort-Object { Get-SecureRandom })
     if ($Secure) { $pw = $pw | ConvertTo-SecureString -AsPlainText -Force }
     return $pw
 }
--- PSModules/RandomPassword/RandomPassword.psm1
+++ PSModules/RandomPassword/RandomPassword.psm1
@@ -27,12 +27,11 @@ function Get-RandomPassword {
     )
 
     $chars = @()
-    do { $chars += ('a'..'z' | Get-Random -Count 1) } until ($chars.Length -gt ($Length * 1 / 4 - 1))
-    do { $chars += ('A'..'Z' | Get-Random -Count 1) } until ($chars.Length -gt ($Length * 2 / 4 - 1))
-    do { $chars += ('0'..'9' | Get-Random -Count 1) } until ($chars.Length -gt ($Length * 3 / 4 - 1))
-    do { $chars += ('!', '@', '#', '$', '%', '&', '=', '+', '-', '?', '^', '*', '.', ',' | Get-Random -Count 1) }
-    until ($chars.Length -gt ($Length - 1))
-    $pw = -join ($chars | Sort-Object { Get-Random })
+    do { $chars += ('a'..'z' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length * 1 / 4 - 1))
+    do { $chars += ('A'..'Z' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length * 2 / 4 - 1))
+    do { $chars += ('0'..'9' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length * 3 / 4 - 1))
+    do { $chars += ([char[]]'!"#$%&()*+,-./:;<=>?@[\]^_`{|}~' | Get-SecureRandom -Count 1) } until ($chars.Length -gt ($Length - 1))
+    $pw = -join ($chars | Sort-Object { Get-SecureRandom })
     if ($Secure) { $pw = $pw | ConvertTo-SecureString -AsPlainText -Force }
     return $pw
 }
