3da4ce0469d899a6390da0bf30a5c6081dcdaf2f
FrankN <fnaerla@outlook.com>
2026-03-17 16:40:25 +0100

    added webhook

--- PSScripts/Start-CandyCountdown.ps1
+++ PSScripts/Start-CandyCountdown.ps1
@@ -1,3 +1,7 @@
+param(
+    [string]$Webhook
+)
+
 # Countdown Until Next Delivery
 $date = Get-Date -Date 11:00:00
 for ($i = 0; $i -le 7; $i++) {
@@ -9,10 +13,10 @@ for ($i = 0; $i -le 7; $i++) {
 }
 $span = New-TimeSpan -Start (Get-Date) -End $nextFriday
 if ($span -gt [timespan]::new(6, 21, 0, 0)) {
-    $msg = 'Hurra!! Det er godteritid :D'
+    $msg = 'Hurra!! Det er godteritid 😊🍬🍭'
 }
 else {
-    $msg = ('Det er {0} dag{1}, {2} time{3} og {4} minutt{5} igjen til godteri.' -f @(
+    $msg = ('Det er {0} dag{1}, {2} time{3} og {4} minutt{5} igjen til godteri. 🍬🍭' -f @(
             $span.Days, ($span.Days -ne 1 ? 'er' : $null),
             $span.Hours, ($span.Hours -ne 1 ? 'r'  : $null),
             $span.Minutes, ($span.Minutes -ne 1 ? 'er' : $null)
@@ -20,3 +24,17 @@ else {
     )
 }
 Write-Output $msg
+
+$json = @{
+    blocks = @(
+        @{
+            type = 'section'
+            text = @{
+                type  = 'plain_text'
+                text  = $msg
+                emoji = $true
+            }
+        }
+    )
+} | ConvertTo-Json -Depth 3
+Invoke-RestMethod -Uri $Webhook -Method Post -Body $json -ContentType 'application/json'
